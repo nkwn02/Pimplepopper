b868a288811bd4ca259f4f5f6ec43d18e7108225
nkwn02 <nathaniel.kwan@sjsu.edu>
2022-04-23 18:59:34 -0700

    3.4-.5
    added all assets and stylization
    
    only thing needed is fixed mousepress function to switch to end game with 'pimplecount'

--- sketch.js
+++ sketch.js
@@ -1,4 +1,20 @@
 let gameState = 'title';
+let pstartImg;
+let pplogo1Img;
+let pplogo2IMG;
+let eye1Img;
+let eye2IMG;
+
+let pimpleCount;
+
+function preload() {
+  pstartImg = loadImage('assets/startpimple.png');
+  pplogo1Img = loadImage('assets/pplogo1.png');
+  pplogo2Img = loadImage('assets/pplogo2.png');
+  eye1Img = loadImage('assets/eye1.png');
+  eye2Img = loadImage('assets/eye2.png');
+}
+
 
 function setup() {
   createCanvas(800, 400);
@@ -35,36 +51,34 @@ if (gameState === 'title' || gameState === 'gameover') {
 }
 
 function titleScreen() {
-background(204, 166, 106);
-stroke(255);
-fill(207, 14, 14);
+background(214,157, 108);
+stroke(192, 117, 89);
+fill(167, 75, 41);
 textSize(100);
 textAlign(CENTER);
-text('Pimple Popper', width*0.5, height*0.33);
 textSize(25);
 text('Pop all the pimples before they CONSUME YOU', width*0.5, height*0.66);
 fill(255);
 textSize(25);
 text('Press the space bar to start', width*0.5, height*.800);
+//image(pstartImg, 375, 280);
+
+image(pplogo1Img, 55, 45);
+image(pplogo2Img, 440, 45);
 }
 
 
 function playScreen() {
-background(204, 166, 106);
+background(214,157, 108);
 
-//face
-fill(255);
-ellipse(250, 150, 45, 45);
-ellipse(650, 150, 45, 45);
+//eyes
+image(eye1Img, 55, 45);
+image(eye2Img, 440, 45);
 
+//mouth
 fill(0);
-ellipse(450, 300, 100, 10)
+ellipse(420, 300, 100, 10)
 
-
-ellipse(250, 150, 35, 35);
-ellipse(650, 150, 35, 35);
-fill(255);
-//end of face code
 
   if(frameCount%10 == 0) {
 
@@ -73,38 +87,70 @@ fill(255);
     var rnd = floor(random(0, 4));
 
     if(rnd == 0)
-      newSprite.addAnimation('img', 'assets/pimple.png');
+      newSprite.addAnimation('img', 'assets/pimple1.png');
     if(rnd == 1)
-      newSprite.addAnimation('img', 'assets/pimple.png');
+      newSprite.addAnimation('img', 'assets/pimple2.png');
     if(rnd == 2)
-      newSprite.addAnimation('img', 'assets/pimple.png');
+      newSprite.addAnimation('img', 'assets/pimple3.png');
     if(rnd == 3)
-      newSprite.addAnimation('img', 'assets/pimple.png');
+      newSprite.addAnimation('img', 'assets/pimple4.png');
+
+
+
+      pimpleCount++;
+
+
+          //newSprite.life = 1000;
+        }
+
+        function mousePressed(){
+          if (mouseX > 1 && mouseX < 800){
+            if (mouseY > -1 && mouseY < 400) {
+      pimpleCount--;
+            }
+          }
+        }
+
+
+      if (PimpleCount = 100) {
+        gameState = 'gameover';
+      }
 
 
     //newSprite.life = 1000;
-  }
+
 
 
   drawSprites();
-
-//this code is temporary cannot figure out how to link amount of sprites to gameover
-  if (mouseX > (width/2)-20 && mouseX < (width/2)+20) {
-    if (mouseY > (height/2)-20 && mouseY < (height/2)+20) {
-      gameState = 'gameover';
-    }
-  }
-
 }
+
+  //this code is temporary cannot figure out how to link amount of sprites to gameover
+    //if (mouseX > (width/2)-20 && mouseX < (width/2)+20) {
+      //if (mouseY > (height/2)-20 && mouseY < (height/2)+20) {
+        //gameState = 'gameover';
+      //}
+    //}
+
+//pop pimple to start (test)
+  //function mousePressed(){
+    //if (mouseX > 1 && mouseX < 800){
+      //if (mouseY > 1 && mouseY < 400) {
+  //gameState = 'gameover';
+    //}
+ //}
+//}
+
+//}
 
 
 function endScreen() {
-  background(204, 166, 106);
-  stroke(255);
-  fill(207, 14, 14);
+  background(214,157, 108);
+  stroke(192, 117, 89);
+  fill(167, 75, 41);
   textSize(40);
   textAlign(CENTER);
   text('YOU WERE CONSUMED', width*0.5, height*0.33);
   textSize(20);
+  fill(255);
   text('Press the space bar to try again', width*0.5, height*0.66);
 }
